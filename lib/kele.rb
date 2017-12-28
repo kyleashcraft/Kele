0c9a4e2eceb1b30e577b737b7d2b2899d1916cca
kyleashcraft <ashcraft.k@husky.neu.edu>
2017-12-27 17:19:13 -0800

    added get_me method to Kele class

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,4 +1,5 @@
 require 'httparty'
+require 'json'
 
 class Kele
   include HTTParty
@@ -10,6 +11,11 @@ class Kele
     if !@auth_token
       p "Incorrect credentials, please try again!"
     end
+  end
+
+  def get_me
+    response = self.class.get('/users/me', headers: { "authorization" => @auth_token })
+    user_data = JSON.parse(response.body)
   end
 
 end
